cf654ca858b46e5193bc151c09a2df01471fc40a
Septolum <Septolum@gmail.com>
2020-01-13 17:14:50 +0000

    Don't import everything Useful for low memory situations

--- library/pimoroni_physical_feather_pins/__init__.py
+++ library/pimoroni_physical_feather_pins/__init__.py
@@ -1,5 +1,5 @@
 from . import pin_error
-from . import m4, m0, nRF52840
+#from . import m4, m0, nRF52840
 #from . import _template
 
 def __getattr__(name):
@@ -31,14 +31,14 @@ import os as _os
 _machine = _os.uname().machine
 
 _case = {
-    'Adafruit Feather M4 Express with samd51j19' : m4,
-    'Adafruit Feather M0 Express with samd21g18' : m0,
-    'Adafruit Feather nRF52840 Express with nRF52840' : nRF52840,
+    'Adafruit Feather M4 Express with samd51j19' : "m4",
+    'Adafruit Feather M0 Express with samd21g18' : "m0",
+    'Adafruit Feather nRF52840 Express with nRF52840' : "nRF52840",
 
-    'Adafruit Feather M0 Adalogger with samd21g18' : m0,
-    'Adafruit Feather M0 Basic with samd21g18' : m0,
-    'Adafruit Feather M0 RFM69 with samd21g18' : m0,
-    'Adafruit Feather M0 RFM9x with samd21g18' : m0,
+    'Adafruit Feather M0 Adalogger with samd21g18' : "m0",
+    'Adafruit Feather M0 Basic with samd21g18' : "m0",
+    'Adafruit Feather M0 RFM69 with samd21g18' : "m0",
+    'Adafruit Feather M0 RFM9x with samd21g18' : "m0",
 
 }
 
@@ -47,6 +47,8 @@ _case = {
 # Case statement replacement
 #[value for key, value in _case.items() if _machine in key][0].init(globals())
 try:
-    _case[_machine].init(globals())
+    mod = __import__(_case[_machine], globals(), locals(), [None], 1)
+    mod.init(globals())
+    #_case[_machine].init(globals())
 except KeyError:
     raise NotImplementedError("Sorry, '{}' is not supported currently".format(_machine))
