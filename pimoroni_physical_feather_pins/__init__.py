c99211a944bded3ec68f4daa2c94798d7d9ab5ef
Phil Howard <phil@gadgetoid.com>
2020-08-10 15:16:58 +0100

    Apply Black reformatting

--- pimoroni_physical_feather_pins/__init__.py
+++ pimoroni_physical_feather_pins/__init__.py
@@ -31,61 +31,73 @@ Implementation Notes
 """
 
 from . import pin_error
-#from . import m4, m0, nRF52840
-#from . import _template
+
+# from . import m4, m0, nRF52840
+# from . import _template
 
 
 __version__ = "0.0.0-auto.0"
-__repo__ = "https://github.com/pimoroni/Pimoroni_CircuitPython_physical_feather_pins.git"
+__repo__ = (
+    "https://github.com/pimoroni/Pimoroni_CircuitPython_physical_feather_pins.git"
+)
 
 
 def __getattr__(name):
     raise pin_error.PinDoesNotExistError("Pin '%s' does not exist" % (name))
 
-#region Consistent Pins
+
+# region Consistent Pins
+
 
 def pin1():
-    raise pin_error.PinNotAddressableError("Pin 1 is not addressable. To reset the board, use 'microcontroller.reset'")
+    raise pin_error.PinNotAddressableError(
+        "Pin 1 is not addressable. To reset the board, use 'microcontroller.reset'"
+    )
+
 
 def pin2():
     raise pin_error.PinNotAddressableError("Pin 2 is not addressable.")
 
+
 def pin4():
     raise pin_error.PinNotAddressableError("Pin 4 is not addressable.")
+
 
 def pin26():
     raise pin_error.PinNotAddressableError("Pin 26 is not addressable.")
 
+
 def pin27():
     raise pin_error.PinNotAddressableError("Pin 27 is not addressable.")
+
 
 def pin28():
     raise pin_error.PinNotAddressableError("Pin 28 is not addressable.")
 
-#endregion Consistent Pins
+
+# endregion Consistent Pins
 
 import os as _os
+
 _machine = _os.uname().machine
 
 _case = {
-    'Adafruit Feather M4 Express with samd51j19' : "m4",
-    'Adafruit Feather M0 Express with samd21g18' : "m0",
-    'Adafruit Feather nRF52840 Express with nRF52840' : "nRF52840",
-
-    'Adafruit Feather M0 Adalogger with samd21g18' : "m0",
-    'Adafruit Feather M0 Basic with samd21g18' : "m0",
-    'Adafruit Feather M0 RFM69 with samd21g18' : "m0",
-    'Adafruit Feather M0 RFM9x with samd21g18' : "m0",
-
+    "Adafruit Feather M4 Express with samd51j19": "m4",
+    "Adafruit Feather M0 Express with samd21g18": "m0",
+    "Adafruit Feather nRF52840 Express with nRF52840": "nRF52840",
+    "Adafruit Feather M0 Adalogger with samd21g18": "m0",
+    "Adafruit Feather M0 Basic with samd21g18": "m0",
+    "Adafruit Feather M0 RFM69 with samd21g18": "m0",
+    "Adafruit Feather M0 RFM9x with samd21g18": "m0",
 }
 
-#m4.init(globals())
+# m4.init(globals())
 
 # Case statement replacement
-#[value for key, value in _case.items() if _machine in key][0].init(globals())
+# [value for key, value in _case.items() if _machine in key][0].init(globals())
 try:
     mod = __import__(_case[_machine], globals(), locals(), [None], 1)
     mod.init(globals())
-    #_case[_machine].init(globals())
+    # _case[_machine].init(globals())
 except KeyError:
     raise NotImplementedError("Sorry, '{}' is not supported currently".format(_machine))
--- pimoroni_physical_feather_pins/pin_error.py
+++ pimoroni_physical_feather_pins/pin_error.py
@@ -3,15 +3,16 @@ class PinNotAddressableError(Exception):
         default_message = "Pin is not addressable"
 
         if not (args or kwargs):
-            args = (default_message, )
-        
+            args = (default_message,)
+
         super().__init__(*args, **kwargs)
+
 
 class PinDoesNotExistError(Exception):
     def __init__(self, *args, **kwargs):
         default_message = "Pin does not exist"
 
         if not (args or kwargs):
-            args = (default_message, )
-        
+            args = (default_message,)
+
         super().__init__(*args, **kwargs)
